fdd5565e37cba0d54d142835c9923ff5bc41f720
Sean Murphy <seanmurphy@Seans-MBP.local>
2021-05-05 08:35:57 +1000

    Update full stack to use self as cross account

--- init.sh
+++ init.sh
@@ -2,17 +2,19 @@
 
 # check if infra bucket exists au-slyp-com-shared-cross-account-template-base-poc or else make it
 
-sam build --template templates/full-stack.template.yaml -b .aws-sam/build --debug
+#sam build --template templates/full-stack.template.yaml -b .aws-sam/build --debug
 
-sam deploy --stack-name=shared-deployments --s3-prefix=shared-deployments --profile=shared-poweruser --s3-bucket=au-slyp-com-shared-cross-account-template-base-poc \
---parameter-overrides=\
-DevAwsAccountId=293952095306 \
-ProdAwsAccountId=040536061213 \
-S3BucketName=au-slyp-com-shared-cross-account-template-base-poc \
-S3KeyPrefix=shared-deployments  \
---capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+#sam deploy --stack-name=shared-deployments --s3-prefix=shared-deployments --profile=shared-poweruser --s3-bucket=au-slyp-com-shared-cross-account-template-base-poc \
+#--parameter-overrides=\
+#DevAwsAccountId=293952095306 \
+#ProdAwsAccountId=040536061213 \
+#S3BucketName=au-slyp-com-shared-cross-account-template-base-poc \
+#S3KeyPrefix=shared-deployments  \
+#--capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
 
 
+aws --profile=shared-poweruser cloudformation package --template-file templates/full-stack.template.yaml --output-template-file full-stack-transformed.yaml --s3-bucket au-slyp-com-shared-cross-account-template-base-poc
+aws --profile=shared-poweruser cloudformation deploy --template full-stack-transformed.yaml --stack-name shared-deployments --parameter-overrides DevAwsAccountId=293952095306 ProdAwsAccountId=040536061213 S3BucketName=au-slyp-com-shared-cross-account-template-base-poc S3KeyPrefix=shared-deployments --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND 
 
 exit 
 
